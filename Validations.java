6d0eaedb1bacadfbdb3e492f8c626bdf3dc06802
Nishi-Gandhi <114444265+Nishi-Gandhi@users.noreply.github.com>
2022-10-31 21:41:49 -0400

    Add files via upload

---
+++ Validations.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Validations;
+
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Validations {
+     public static boolean ValidateName(String Name) {
+        if (!Pattern.compile("^[a-zA-Z]+( [a-zA-Z]+)*$").matcher(Name).matches()) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public static boolean ValidateID(String PatientID) {
+        if (!Pattern.compile("\\d{4}").matcher(PatientID).matches()) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public static boolean ValidateAge(String Age) {
+        if (!Pattern.compile("\\d{2}").matcher(Age).matches()) {
+
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean ValidatePassword(String Password) {
+        if (!Pattern.compile("^[0-9a-zA-Z]{6}").matcher(Password).matches()) {
+
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean ValidateAddress(String Address) {
+        if (!Pattern.compile("^[a-zA-Z0-9_]+$").matcher(Address).matches()) {
+
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean ValidateCommunity(String Community) {
+        if (!Pattern.compile("^[a-zA-Z]+$").matcher(Community).matches()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean ValidateContactNumber(String ContactNumber) {
+        if (!Pattern.compile("\\d{10}").matcher(ContactNumber).matches()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean ValidateEmail(String Email) {
+        if (!Pattern.compile("^([a-zA-Z0-9]+)([\\.{1}])?([a-zA-Z0-9]+)\\@gmail([\\.])com").matcher(Email).matches()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean ValidateCity(String City) {
+        if (!Pattern.compile("^[a-zA-Z]+$").matcher(City).matches()) {
+
+            return false;
+        }
+        return true;
+    }
+
+}
+
+
